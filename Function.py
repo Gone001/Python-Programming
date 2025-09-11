e5e1447c3fb98094ed9b0f31bb294bbada5fe590
Gone001 <2024D2R034@mietjammu.in>
2025-09-11 11:23:12 +0530

    Create Function.py
    First function

---
+++ Function.py
@@ -0,0 +1,12 @@
+def add(num1):
+    if(num1>0):
+        if (num1%2==0):
+            print("No is + even ")
+        else:print("No is odd ")
+    if(num1<0):
+        if(num1%-2==0):
+            print("No is - even ")
+        else:
+            print("No is - odd ")
+num1=int (input("Enter the no "))
+add(num1)
